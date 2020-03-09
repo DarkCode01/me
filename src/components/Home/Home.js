6278b4e0d5c1fbac55f41e275204a4535fc3a16b
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-03-09 16:16:05 -0400

    feat: add image of all my profiles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 
 export const Home = () => (
   <header className="app-header">
+    <img
+      src="https://avatars0.githubusercontent.com/u/23442814?s=400&u=3f04d79827b9513c03de3288dc7e801df35da55a&v=4"
+      className="App-logo"
+      alt="logo"
+    />
     <p className="typing-effect">
       José Miguel Segura Polanco &#8287;
       <a
@@ -17,6 +22,6 @@ export const Home = () => (
     <Link
       className="open-terminal"
       to="/terminal"
-    > Open terminal</Link>
+    > Open Terminal</Link>
   </header>
 );
