67a6916ab04e151afdd2275376f4b15018b236dc
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-03-09 16:24:54 -0400

    feat: remove react helmet of title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
-import Helmet from 'react-helmet';
 import { Home } from './components/Home';
 import { Terminal } from './components/Terminal';
 
@@ -14,14 +13,6 @@ import './styles/App.css';
 function App() {
   return (
     <div className="app">
-      <Helmet>
-        <title>Me - Website!</title>
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://avatars0.githubusercontent.com/u/23442814?s=400&u=3f04d79827b9513c03de3288dc7e801df35da55a&v=4"
-        />
-      </Helmet>
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
