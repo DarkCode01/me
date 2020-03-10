4ed0553082ea1bf693ff1111d957ed186510cb0a
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-03-09 22:31:45 -0400

    feat: update image of home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 export const Home = () => (
   <header className="app-header">
     <img
-      src="https://avatars0.githubusercontent.com/u/23442814?s=400&u=3f04d79827b9513c03de3288dc7e801df35da55a&v=4"
+      style={{ width: '50%' }}
+      src="darkcoder.jpg"sizes
       className="App-logo"
       alt="logo"
     />
