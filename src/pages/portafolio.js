74d145a698591bc9bc14c35ff4fefd69b56c5579
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-05-24 21:14:16 -0400

    feat: added option to move folders and executables on screen

--- src/pages/portafolio.js
+++ src/pages/portafolio.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
-import DragSelect from 'dragselect';
 import { Layout } from 'antd';
+import DragSelect from 'dragselect';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Portafolio as PortafolioComponent } from '../components';
 import { FolderFilled, CodeFilled } from '@ant-design/icons';
 
@@ -21,25 +23,23 @@ class Portafolio extends Component {
     // effect of select objects
     new DragSelect({
       selectables: document.querySelectorAll('.item'),
-      area: document.querySelector('#desktop')
+      area: document.querySelector('.desktop')
     });
   }
 
   render() {
     return (
-      <Layout id="desktop" style={{ backgroundColor: 'transparent' }}>
-        <Layout.Content style={{ padding: '50px' }}>
-          <PortafolioComponent.WindowComponent />
+      <Layout style={{ backgroundColor: 'transparent' }}>
+        <Layout.Content>
+          <DndProvider backend={HTML5Backend}>
+            <PortafolioComponent.FoldersComponent
+              folders={{
+                folder: { top: 20, left: 20, icon: <FolderFilled />, name: 'Me - Files', open: '' },
+                exec: { top: 180, left: 20, icon: <CodeFilled />, name: 'terminal', open: '' }
+              }}
+            />
+          </DndProvider>
         </Layout.Content>
-        <Layout.Sider theme="dark" breakpoint="sm" style={{ backgroundColor: 'transparent' }}>
-          {/* Folders and executables */}
-          <PortafolioComponent.FoldersComponent
-            folders={[
-              { icon: <FolderFilled />, name: 'Me - Files', open: '' },
-              { icon: <CodeFilled />, name: 'terminal', open: '' }
-            ]}
-          />
-        </Layout.Sider>
       </Layout>
     );
   }
