9fe1f9c9fcbdf0cf321aee935b95880c90d886f8
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2020-05-26 00:44:46 -0400

    feat: create function to manage pids

--- src/pages/portafolio.js
+++ src/pages/portafolio.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import update from 'immutability-helper';
+import { createPID } from '../utils/helpers-functions';
+
 
 import { Layout } from 'antd';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { Portafolio as PortafolioComponent } from '../components';
-import { FolderFilled, CodeFilled } from '@ant-design/icons';
+import { FolderFilled, CodeFilled, FilePdfOutlined } from '@ant-design/icons';
 
 
 class Portafolio extends Component {
@@ -13,19 +15,21 @@ class Portafolio extends Component {
     super();
 
     this.state = {
-      data: {
+      pids: {
         terminal: { top: 120, left: 20, icon: <CodeFilled />, name: 'terminal', open: '' },
         folder: {
           top: 20,
           left: 20,
           icon: <FolderFilled />,
           name: 'Me - Files',
-          open: this.openNewWindow.bind(this)({ title: 'no', files: [null, null]})
+          open: this.openWindowManageFiles.bind(this)({ title: 'no', files: [null, null]})
         }
       }
     }
 
     this.moveBox = this.moveBox.bind(this);
+    this.createPID = this.createPID.bind(this);
+    this.killProccess = this.killProccess.bind(this);
   }
 
   componentDidMount() {
@@ -34,24 +38,56 @@ class Portafolio extends Component {
     document.body.style.backgroundImage = 'url(darkcoder.jpg)';
   }
 
-  openNewWindow({ files, title }) {
+  createPID() {
+    const pid = createPID();
+
+    // registry process
+    this.setState({ pids: { ...this.state.pids, [`${pid}`]: {} } });
+
+    return pid;
+  }
+
+  openWindowManageFiles({ files, title }) {
     return () => {
-      this.setState(({ data }) => ({
-        data: {
-          ...data,
-          window: {
-            top: 20, left: 150,
-            name: 'gfhgfghfhg', open: '',
-            icon: <PortafolioComponent.WindowComponent title={title} files={files} />
+      const pid = this.createPID();
+
+      this.setState(({ pids }) => ({
+        pids: {
+          ...pids,
+          [`${pid}`]: {
+            top: 20,
+            left: 150,
+            name: pid,
+            open: '',
+            icon: <PortafolioComponent.WindowComponent
+              title={title}
+              close={() => {
+                this.killProccess(pid);
+              }}
+              children={ files.map(file => (
+                <FilePdfOutlined
+                  key={Math.random() * 100}
+                  className="window-files"
+                  style={{ fontSize: '500%' }}
+                />
+              ))}
+            />
           }
         }
       }));
     }
   }
 
+  killProccess(pid) {
+    // remove explicit process
+    delete this.state.pids[`${pid}`];
+
+    this.setState({ pids: { ...this.state.pids }});
+  }
+
   moveBox(id, left, top) {
-    this.setState(({ data }) => ({
-      data: update(this.state.data, {
+    this.setState(({ pids }) => ({
+      pids: update(this.state.pids, {
         [id]: {
           $merge: { left, top },
         },
@@ -65,7 +101,7 @@ class Portafolio extends Component {
         <Layout.Content>
           <DndProvider backend={HTML5Backend}>
             <PortafolioComponent.FoldersComponent
-              folders={this.state.data}
+              folders={this.state.pids}
               moveBox={this.moveBox}
             />
           </DndProvider>
